4ba890e9f2315cb2957b8f40700ea49d828240cb
helen0215 <seocc0604@gmail.com>
2022-02-04 11:23:44 +0900

    hooks 사용하기

--- 17. react-redux-tutorial/src/containers/CounterContainer.js
+++ 17. react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,28 +1,20 @@
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
+import { useCallback } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import Counter from "../components/Counter";
 import { decrease, increase } from '../modules/counter';
 
-const CounterContainer = ({number, increase, decrease}) => {
-  return <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+const CounterContainer = () => {
+  const number = useSelector(state => state.counter.number);
+  const dispatch = useDispatch();
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
+  return (
+    <Counter
+      number={number}
+      onIncrease={onIncrease}
+      onDecrease={onDecrease}
+    />
+  );
 };
 
-const mapStateToProps = state => ({
-  number: state.counter.number,
-});
-
-const mapDispatchToProps = dispatch => (
-  // { increase: () => dispatch(increase()),
-  // decrease: () => dispatch(decrease()),}
-  bindActionCreators({
-    increase,
-    decrease,
-  },
-  dispatch
-));
-
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(CounterContainer);
+export default CounterContainer;
--- 17. react-redux-tutorial/src/containers/TodosContainer.js
+++ 17. react-redux-tutorial/src/containers/TodosContainer.js
@@ -1,36 +1,39 @@
-import { connect } from "react-redux";
+import React from 'react';
+import { useSelector, useDispatch } from "react-redux";
 import Todos from "../components/Todos";
 import { changeInput, toggle, insert, remove } from '../modules/todos';
+import useActions from '../lib/useActions';
 
-const TodosContainer = ({
-  input,
-  todos,
-  changeInput,
-  insert,
-  toggle,
-  remove,
-}) => {
+const TodosContainer = () => {
+  const {input, todos} = useSelector(({todos}) => ({
+    input: todos.input,
+    todos: todos.list,
+  }));
+  const dispatch = useDispatch();
+
+  // useActions 사용하기
+  // const onChangeInput = useCallback(input => dispatch(changeInput(input)), [dispatch]);
+  // const onInsert = useCallback(text => dispatch(insert(text)), [dispatch]);
+  // const onToggle = useCallback(id => dispatch(toggle(id)), [dispatch]);
+  // const onRemove = useCallback(id => dispatch(remove(id)), [dispatch]);
+  const [onChangeInput, onToggle, onInsert, onRemove] = useActions(
+    [changeInput, toggle, insert, remove], 
+    []
+  );
+
   return (
     <Todos
       input={input}
       todos={todos}
-      onChangeInput={changeInput}
-      onInsert={insert}
-      onToggle={toggle}
-      onRemove={remove}
+      onChangeInput={onChangeInput}
+      onInsert={onInsert}
+      onToggle={onToggle}
+      onRemove={onRemove}
     />
   );
 };
 
-export default connect(
-  ({todos}) => ({
-    input: todos.input,
-    todos: todos.list,
-  }),
-  {
-    changeInput,
-    insert,
-    toggle,
-    remove,  
-  }
-)(TodosContainer);
+// connect는 props가 바꾸지 않을때 리렌더링을 자동으로 방지해주지만 
+// useSelector를 사용하는 경우 React.memo로 최적화 작업을 해주어야함
+// export default TodosContainer;
+export default React.memo(TodosContainer);
