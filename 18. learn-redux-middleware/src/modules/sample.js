b0e9ab4ba8412fce7bb27b4f36f5ab63423fbee2
helen0215 <seocc0604@gmail.com>
2022-02-12 19:31:25 +0900

    thunk 함수 생성 부분 줄이기

--- 18. learn-redux-middleware/src/modules/sample.js
+++ 18. learn-redux-middleware/src/modules/sample.js
@@ -1,5 +1,6 @@
 import { handleActions } from 'redux-actions';
 import * as api from '../lib/api';
+import createRequestThunk from '../lib/createRequestThunk';
 
 const GET_POST = 'sample/GET_POST';
 const GET_POST_SUCCESS = 'sample/GET_POST_SUCCESS';
@@ -9,47 +10,8 @@ const GET_USERS = 'sample/GET_USERS';
 const GET_USERS_SUCCESS = 'sample/GET_USERS_SUCCESS';
 const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 
-// thunk 함수를 생성합니다
-// thunk 함수는 내부에서 시작할 때, 성공했을 때, 실패했을 때 다른 액션을 디스패치 합니다
-export const getPost = id => async dispatch => {
-  dispatch({type: GET_POST}); // 요청을 시작한 것을 알림
-  try {
-    const response = await api.getPost(id);
-    
-    dispatch({
-      type: GET_POST_SUCCESS,
-      payload: response.data
-    }); // 요청 성공
-
-  } catch (e) {
-    dispatch({
-      type: GET_POST_FAILURE,
-      payload: e,
-      error: true
-    }); // 에러 발생
-    throw e; // 나중에 컴포넌트단에서 에러를 조회할 수 있게 해줌
-  }
-};
-
-export const getUsers = () => async dispatch => {
-  dispatch({type: GET_USERS}); // 요청을 시작한 것을 알림
-  try {
-    const response = await api.getUsers();
-    
-    dispatch({
-      type: GET_USERS_SUCCESS,
-      payload: response.data
-    }); // 요청 성공
-
-  } catch (e) {
-    dispatch({
-      type: GET_USERS_FAILURE,
-      payload: e,
-      error: true
-    }); // 에러 발생
-    throw e; // 나중에 컴포넌트단에서 에러를 조회할 수 있게 해줌
-  }
-};
+export const getPost = createRequestThunk(GET_POST, api.getPost);
+export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
 
 // 초기 상태
 const initialState = {
