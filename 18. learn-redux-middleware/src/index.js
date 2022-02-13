eb30bf8a9b0873c113fe21ba6b70aa57285bd966
helen0215 <seocc0604@gmail.com>
2022-02-13 15:54:47 +0900

    redux-saga counterSaga 적용

--- 18. learn-redux-middleware/src/index.js
+++ 18. learn-redux-middleware/src/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { applyMiddleware, createStore } from 'redux';
 import App from './App';
-import rootReducer from './modules/index';
+import rootReducer, { rootSaga } from './modules/index';
 import { Provider } from 'react-redux';
 import { createLogger } from 'redux-logger';
 import ReduxThunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const logger = createLogger();
-const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer, 
+  composeWithDevTools(applyMiddleware(logger, ReduxThunk, sagaMiddleware))
+);
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <React.StrictMode>
