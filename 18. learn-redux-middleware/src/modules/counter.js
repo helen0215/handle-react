05159ab232b027410df7348e9403b48156e4be68
helen0215 <seocc0604@gmail.com>
2022-02-13 16:25:53 +0900

    redux-saga select, throttle

--- 18. learn-redux-middleware/src/modules/counter.js
+++ 18. learn-redux-middleware/src/modules/counter.js
@@ -1,5 +1,5 @@
 import { createAction, handleActions } from 'redux-actions';
-import { delay, put, takeEvery, takeLatest } from 'redux-saga/effects';
+import { delay, put, takeEvery, takeLatest, select, throttle } from 'redux-saga/effects';
 
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
@@ -16,6 +16,10 @@ export const decreaseAsync = createAction(DECREASE_ASYNC, () => undefined);
 function* increaseSaga() {
   yield delay(1000);
   yield put(increase());
+
+  // saga에서 현재 상태 조회하기 
+  const number = yield select(state => state.counter);
+  console.log(`현재 값은 ${number}입니다`);
 }
 
 function* decreaseSaga() {
@@ -25,7 +29,10 @@ function* decreaseSaga() {
 
 export function* counterSaga() {
   // takeEvery는 들어오는 모든 액션에 대해 특정 작업을 처리해줌
-  yield takeEvery(INCREASE_ASYNC, increaseSaga);
+  // yield takeEvery(INCREASE_ASYNC, increaseSaga);
+
+  // 첫 번째 파라미터 : n초 * 1000
+  yield throttle(3000, INCREASE_ASYNC, increaseSaga);
 
   // takeLatest는 기존에 진행중이던 작업이 있다면 취소초리하고
   // 가장 마지막으로 실행된 작업만 수행
