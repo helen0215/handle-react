32b3ebc086a843e1565e0bad36c135ce04276f7e
helen0215 <seocc0604@gmail.com>
2022-03-17 22:08:45 +0900

    Joi를 이용한 write, update validation check

--- 21. blog/blog-backend/src/api/posts/posts.ctrl.js
+++ 21. blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -1,5 +1,6 @@
 import Post from '../../models/post';
 import mongoose from 'mongoose';
+import Joi from 'joi';
 
 const {ObjectId} = mongoose.Types;
 
@@ -17,6 +18,22 @@ POST /api/posts
 {title, body}
 */
 export const write = async ctx => {
+  const schema = Joi.object().keys({
+    title: Joi.string().required(), // required() 가 있으면 필수 항목
+    body: Joi.string().required(),
+    tags: Joi.array()
+      .items(Joi.string())
+      .required(), // 문자열로만 이루어진 배열
+  });
+  
+  // 검증하고 나서 실패인 경우 처리
+  const result = schema.validate(ctx.request.body);
+  if (result.error) {
+    ctx.status = 400; // bad request
+    ctx.body = result.error;
+    return;
+  }
+   
   // REST API의 Request Body는 ctx.request.body 에서 조회할 수 있음
   const {title, body, tags} = ctx.request.body;
   const post = new Post({
@@ -79,6 +96,21 @@ PATCH /api/posts/:id
 */
 export const update = async ctx => {
   const {id} = ctx.params;
+  
+  // required가 없음
+  const schema = Joi.object().keys({
+    title: Joi.string(), 
+    body: Joi.string(),
+    tags: Joi.array().items(Joi.string()),
+  });
+
+  const result = schema.validate(ctx.request.body);
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
+  
   try {
     const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
       new: true, // 이 값을 설정하면 업데이트 된 데이터를 반환함, false 일경우 업데이트 되기 전의 데이터를 반환
