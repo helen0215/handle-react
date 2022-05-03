a116b486aa25aa0b38dd167744b3a8d31a09824d
helen0215 <seocc0604@gmail.com>
2022-05-03 20:49:13 +0900

    회원가입, 로그인 redux-saga 적용

--- 21. blog/blog-frontend/src/index.js
+++ 21. blog/blog-frontend/src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
-import rootReducer from './modules';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer, {rootSaga} from './modules';
 
-const store = createStore(rootReducer, composeWithDevTools());
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer, 
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
+
+sagaMiddleware.run(rootSaga);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- 21. blog/blog-frontend/src/modules/auth.js
+++ 21. blog/blog-frontend/src/modules/auth.js
@@ -1,7 +1,18 @@
 import { createAction, handleActions } from 'redux-actions';
+import createRequestSaga, { createRequestActionTypes } from '../lib/saga/createRequestSaga';
+import { takeLatest } from 'redux-saga/effects';
+import * as authAPI from '../lib/api/auth';
 
 const CAHNGE_FIELD = 'auth/CHANGE_FIELD';
 const INITALIZE_FORM = 'auth/INITALIZE_FORM';
+
+const [REGISTER, REGISTER_SUCCESS, REGISTER_FAILURE] = createRequestActionTypes(
+  'auth/REGISTER',
+);
+
+const [LOGIN, LOGIN_SUCCESS, LOGIN_FAILURE] = createRequestActionTypes(
+  'auth/LOGIN',
+);
 
 export const changeField = createAction(
   CAHNGE_FIELD,
@@ -14,6 +25,24 @@ export const changeField = createAction(
 
 export const initializeForm = createAction(INITALIZE_FORM, form => form); // register / login
 
+export const register = createAction(REGISTER, ({username, password}) => ({
+  username,
+  password,
+}));
+
+export const login = createAction(LOGIN, ({username, password}) => ({
+  username,
+  password,
+}));
+
+// saga 생성
+const registerSaga = createRequestSaga(REGISTER, authAPI.register);
+const loginSaga = createRequestSaga(LOGIN, authAPI.login);
+export function* authSaga() {
+  yield takeLatest(REGISTER, registerSaga);
+  yield takeLatest(LOGIN, loginSaga);
+}
+
 const initialState = {
   register: {
     username: '',
@@ -24,6 +53,8 @@ const initialState = {
     username: '',
     password: '',
   },
+  auth: null,
+  authError: null,
 };
 
 const auth = handleActions(
@@ -40,7 +71,29 @@ const auth = handleActions(
       [form]: {
         ...initialState[form]
       }
-    })
+    }),
+    // 회원가입 성공
+    [REGISTER_SUCCESS]: (state, {payload: auth}) => ({
+      ...state,
+      authError: null,
+      auth,
+    }),
+    // 회원가입 실패
+    [REGISTER_FAILURE]: (state, {payload: error}) => ({
+      ...state,
+      authError: error,
+    }),
+    // 로그인 성공
+    [LOGIN_SUCCESS]: (state, {payload: auth}) => ({
+      ...state,
+      authError: null,
+      auth,
+    }),
+    // 로그인 실패
+    [LOGIN_FAILURE]: (state, {payload: error}) => ({
+      ...state,
+      authError: error,
+    }),
   },
   initialState,
 );
